a33bb6cadaf867d58a4e2cd803ca3c13752ce7dd
Jaisson Valbuena <jaissonvalbuena29@outlook.com>
2022-10-21 11:54:31 -0500

    init to create the contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import '../css/pageCss/contact.css'
 
 const contact = () => {
     return(
-        <h1>desde el contact</h1>
+        <div className="containerContact">
+            <div className="contact-content">
+                <div className="contentContact-info">
+                    <h2>Contact Us</h2>
+                    <h3>monday-Sunday: 7:00am - 5:00pm</h3>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
