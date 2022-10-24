56ef4aadd718f818e188897d8ccda9a9f734a4d5
Jaisson Valbuena <jaissonvalbuena29@outlook.com>
2022-10-23 19:13:29 -0500

    add form into contact page

---
+++ src/components/contactComponents/contactForm.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../../css/contactCss/form.css'
+
+const contactForm = () => {
+    return (
+        <div className="contactForm-content">
+            <div className="contentContact-form">
+                <form action="">
+                    <label htmlFor="fullName">FULLNAME
+                        <br/>
+                        <input type="text" name="email" id="email" placeholder="Enter your fullname"/>
+                    </label>
+                    <br/>
+                    <label htmlFor="yourEmail">YOUR EMAIL
+                        <br/>
+                        <input type="email" placeholder="Enter your email"/>
+                    </label>
+                    <br/>
+                    <label htmlFor="commentary">COMMENTARY
+                        <br/>
+                        <textarea name="commentary" id="commentary" cols="30" rows="10" placeholder="Write anything you want to tell us"></textarea>
+                    </label>
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default contactForm
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
-import '../css/pageCss/contact.css'
+
+import ContactInfo from '.././components/contactComponents/contactInfo'
+import ContactForm from '.././components/contactComponents/contactForm.jsx'
+
 
 const contact = () => {
     return(
-        <div className="containerContact">
-            <div className="contact-content">
-                <div className="contentContact-info">
-                    <h2>Contact Us</h2>
-                    <h3>Monday-Sunday: 7:00am - 5:00pm</h3>
-                    <div className="contact-info">
-                        <p>Email: SmileTask@gmail.com</p>
-                        <p>Telefono: 3112292169</p>
-                    </div>
-                    <section className="contact-foundUs">
-                        Encuentranos en nuestras redes
-                    </section>
-                    <div className="contact-icons">
-                        <a href="https://www.linkedin.com/in/camilo-andres-moreno-roa-9981ba169/" target="_black"><i className='bx bxl-linkedin-square'></i></a>
-                        <a href="https://www.instagram.com/taurodev/" target="_blanck"><i className='bx bxl-instagram-alt'></i></a>
-                        <a href="https://github.com/Camilo1423" target="_blanck"><i className='bx bxl-github' ></i></a>
-                    </div>
-                    <div className=""></div>
-                </div>
-            </div>
+        <div className="contactContainer">
+            <ContactInfo/>
+            <ContactForm/> 
         </div>
     )
 }
