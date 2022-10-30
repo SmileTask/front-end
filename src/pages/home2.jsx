d34ed1ec373cef50940b3bec1607ba7f1fd3317a
ANGIEGIS97 <angiedeconejito@gmail.com>
2022-10-29 21:48:50 -0500

    error imagen

--- src/pages/home2.jsx
+++ src/pages/home2.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import image from '../assets/agradecimientos.jpg'
 import '../css/pageCss/home.css'
 const Home2 = () =>{
     return (
         <div className="container">
-             <h3 className="Agradecimientos">Agradecimientos especiales</h3>
-             <h3 className='ConNostros'>Con nosotros aprenderas las <br></br>bases para el trabajo en equipo prospero</h3>
+            <h3 className="Agradecimientos">Agradecimientos especiales</h3>
+            <image/>
+            <h3 className='ConNostros'>Con nosotros aprenderas las <br></br>bases para el trabajo en equipo prospero</h3>
         </div>
     )
 }
